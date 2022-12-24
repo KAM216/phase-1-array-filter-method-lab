3d7273f256831f9bcdc88d87c29d011f33439d00
khalid <khalid.a.montes@gmail.com>
2022-12-23 23:33:52 -0500

    commit tree 174a6bd953d6608356f32d304e858dcac686bd26

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function matchName(drivers,name){
+    return drivers.filter((drivers) => drivers.name === name);
+}
+
+function findMatching(drivers,name){
+    return drivers.filter((maybe) => maybe.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter((maybe)=> maybe.indexOf(name) === 0);
+}
